407352012a1553417d2f7053214e097d35174876
shivansh aggarwal <shivneeraj2004@gmail.com>
2024-09-15 23:55:34 +0530

    done

--- src/components/aboutpage/Hero.tsx
+++ src/components/aboutpage/Hero.tsx
@@ -8,8 +8,8 @@ export default function Hero() {
                     <Image src="/svg/frame 14.svg" alt="fadsf" fill objectFit="cover" objectPosition='top'></Image>
                 </div>
                 <div className="relative z-[5] flex flex-col items-start justify-center h-[500px] w-[90%] gap-4 mx-auto">
-                    <h1 className=" text-[50px] text-white">About Us</h1>
-                    <p className=" text-[#CFCFCF] text-[25px] w-[60%]">Join the green Revolution by turning agricultural waste into valuable resources for a cleaner , tech-driven future.</p>
+                    <h1 className=" text-[40px] sm:text-[50px] text-white font-bold ">About Us</h1>
+                    <p className=" text-[#CFCFCF] text-[22px] font-medium sm:text-[25px] w-[70%] sm:w-[60%]">Join the green Revolution by turning agricultural waste into valuable resources for a cleaner , tech-driven future.</p>
                     <Button className="bg-primary-green text-white" size={"lg"} >Explore</Button>
 
                 </div>
