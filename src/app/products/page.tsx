be4d450735f845ee47d031c709d9dde4fc83ed2b
somya bhatt <somyacu1@gmail.com>
2024-09-17 14:46:33 +0530

    fix: configs

--- src/app/products/page.tsx
+++ src/app/products/page.tsx
@@ -5,8 +5,17 @@ export default async function Page() {
 
     const products = await getAllProducts();
 
+    if(products.success && products.products) {
+        return (
+            <Product products={products.products} />
+        )
+    }
+
     return (
-        // @ts-ignore
-        <Product products={products.products!} />
+        <div>
+            <h1>Products</h1>
+            <p>There was an error fetching the products</p>
+        </div>
     )
+
 }
